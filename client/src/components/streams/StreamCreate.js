8254083f9dbc257c7f553a344271e7606af9207a
Nishan Acharya <nishan.acharya@gmail.com>
2020-04-26 19:55:19 -0400

    Forms for stream and rest request

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm} from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions/index';
 
 class StreamCreate extends React.Component {
 
@@ -24,8 +26,8 @@ class StreamCreate extends React.Component {
         )
     }
 
-    onSubmit(formValues) {
-        
+    onSubmit = formValues => {
+        this.props.createStream(formValues)
     }
 
     render() {
@@ -53,7 +55,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'createStream',
     validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
