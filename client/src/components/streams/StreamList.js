27f281cd30ad34cc35b1cfebefa369d07c454999
Nishan Acharya <nishan.acharya@gmail.com>
2020-05-11 22:00:37 -0400

    Adds basic auth for edit/delete

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
 
@@ -8,10 +9,38 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    adminAuth(stream){
+        if(stream.userId === this.props.currentUserId){
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    }
+
+    renderCreate(){
+        if(this.props.isSignedIn){
+            return(
+                <div style= {{ textAlign: 'right'}}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
     renderList(){
         return this.props.streams.map(stream => {
             return(
                 <div className="item" key={stream.id}>
+                    <div>{this.adminAuth(stream)}</div>
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -28,13 +57,18 @@ class StreamList extends React.Component {
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         )
     }
 }
 
 const mapStateToProps = (state) => {
-    return { streams : Object.values(state.streams) }
+    return { 
+        streams : Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    }
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
